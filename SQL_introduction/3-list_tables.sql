980966ef144a7b16118d7823a6d364d65399ff3f
dukenyabuti <99273249+dukenyabuti@users.noreply.github.com>
2023-08-16 10:19:13 +0300

    SQL introduction

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- the following script will be used to list all the tables in my SQL server
+
+USE hbtn_0c_0;
+
+SHOW TABLES;
---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- this script will create a new table in my data base
+
+USE hbtn_0c_0;
+CREATE TABLE IF NOT EXISTS first_table(
+    id INT,
+    name VARCHAR(256),
+);
