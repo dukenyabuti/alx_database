e06b317186859ab7574ed3e11202433c3603908a
dukenyabuti <99273249+dukenyabuti@users.noreply.github.com>
2023-08-21 07:11:56 +0300

    more SQL

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+--this script creates a database and a user
+CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
+
+USE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH user_0d_2_pwd;
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+--this scripts creates a table and insterts some values to ir
+
+CREATE TABLE IF NOT EXISTS force_name(
+    id INT
+    name VARCHAR(256) TEXT NOT NULL
+);
